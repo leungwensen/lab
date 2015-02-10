62c0dd037f801d1acf81c770273d0261405596b4
绝云 <wensen.lws@alibaba-inc.com>
2015-02-10 17:39:14 +0800

    atom-shell

---
+++ atom-shell/hello-app/main.js
@@ -0,0 +1,45 @@
+/* jshint undef: true, unused: true */
+/* global require, process, __dirname */
+
+var app = require('app'), // Module to control application life.
+    BrowserWindow = require('browser-window'), // Module to create native browser window.
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the javascript object is GCed.
+    // {
+        mainWindow = null;
+    // }
+
+require('crash-reporter').start(); // Report crashes to our server.
+
+app.on('window-all-closed', function() {
+    // Quit when all windows are closed.
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    // This method will be called when atom-shell has done everything
+    // initialization and ready for creating browser windows.
+
+    // Create the browser window. {
+        mainWindow = new BrowserWindow({
+            width  : 800,
+            height : 600
+        });
+    // }
+
+    // and load the index.html of the app. {
+        mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    // }
+
+    // Emitted when the window is closed. {
+        mainWindow.on('closed', function() {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+    // }
+});
+
