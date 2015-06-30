8a9c5d16d8bd8956a18e0203e4d8a5c8e304996c
绝云 <wensen.lws@alibaba-inc.com>
2015-07-01 01:12:10 +0800

    zfinder is ready to be independent

---
+++ zfinder/readme.markdown
@@ -0,0 +1,10 @@
+# zfinder
+
+[zfinder](https://github.com/leungwensen/lab/tree/master/zfinder) is designed to replace the finder.app in Mac (or file explorer in Windows). with the magic of modern Browsers, we can do a lot more.
+
+## usage
+
+```shell
+zfinder .
+```
+
---
+++ zfinder/server/utils/help.js
@@ -0,0 +1,9 @@
+/* jshint strict: false, undef: true, unused: true */
+/* global module */
+
+module.exports = {
+    '-help(h)': 'print this',
+    '-version(v)': 'print version',
+    '-root(r)': 'set root directory of the server',
+    '-port(p)': 'set port of the server',
+};
